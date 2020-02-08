ae8d5a01ae617727d6c67dbeed44952f2b0ae81e
Varun Varma <56763466+vrvar@users.noreply.github.com>
2020-02-08 11:30:51 -0500

    Format changes

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,28 +1,16 @@
 package frc.robot.subsystems;
-
-
-
-import frc.robot.Constants;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
 import edu.wpi.first.networktables.NetworkTable;
-
 import edu.wpi.first.networktables.NetworkTableEntry;
-
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-
-
-
 public class Limelight extends SubsystemBase {
-
-  public Limelight(final int c, final int s){
-      setMode(c);
-      setStream(s);
+  
+  public Limelight(final int c, final int s) {
+    setMode(c);
+    setStream(s);
   }
 
   @Override
@@ -32,29 +20,29 @@ public class Limelight extends SubsystemBase {
     final NetworkTableEntry ty = table.getEntry("ty");
     final NetworkTableEntry ta = table.getEntry("ta");
 
-    //read values periodically
+    // read values periodically
     final double x = tx.getDouble(0.0);
     final double y = ty.getDouble(0.0);
     final double area = ta.getDouble(0.0);
 
-    //Post to smart dashboard periodically
+    // Post to smart dashboard periodically
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
   }
 
-  public void setMode(final int m){
-      NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(m);
+  public void setMode(final int m) {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(m);
   }
 
-  public void setStream(final int m){
+  public void setStream(final int m) {
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").setNumber(m);
   }
 
   public int getStream() {
     return (int) NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").getDouble(0.0);
   }
-  
+
   public int getMode() {
     return (int) NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").getDouble(0.0);
   }
