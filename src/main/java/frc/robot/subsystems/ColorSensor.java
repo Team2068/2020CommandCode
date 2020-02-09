bc8f128bdec76eef289d00d5a4b4437e97fae1d0
Andrew Schineller <schineaj23@pwcs-edu.org>
2020-02-09 14:39:35 -0500

    Added code for getting color from FSM and input color manually

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -8,8 +8,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.util.Color;
@@ -24,32 +29,77 @@ public class ColorSensor extends SubsystemBase {
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     private final ColorMatch m_colorMatcher = new ColorMatch();
 
+    private final Color NullTarget = ColorMatch.makeColor(0.0, 0.0, 0.0);
     private final Color BlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
     private final Color GreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
     private final Color RedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
     private final Color YellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-    String colorString;
-    SendableChooser<Color> colorChooser = new SendableChooser<>();
+    private String colorString;
+    private SendableChooser<Color> colorChooser = new SendableChooser<>();
+    private ShuffleboardTab dashboardTab = Shuffleboard.getTab("SmartDashboard");
+ 
+    private Color getColorMatch(){ // actually get real color from fms
+      String gameData;
+      gameData = DriverStation.getInstance().getGameSpecificMessage();
+      if(gameData.length() > 0)
+      {
+        switch (gameData.charAt(0))
+        {
+          case 'B' :
+            return BlueTarget;
+          case 'G' :
+            return GreenTarget;
+          case 'R' :
+            return RedTarget;
+          case 'Y' :
+            return YellowTarget;
+          default :
+            DriverStation.reportWarning("Corrupt Color Data Recieved from FMS", false); //This is corrupt data
+            break;
+        }
+      } else {
+        DriverStation.reportWarning("No Color Data Receieved from FMS", false);
+        //Code for no data received yet
+      }
+      return NullTarget;
+    }
+
+    private void createColorChooser(){
+      SendableRegistry.add(colorChooser, "Color Chooser");
+      SendableRegistry.setName(colorChooser, "Color Chooser");
+
+      colorChooser.setDefaultOption("None", NullTarget);   
+      colorChooser.addOption("Blue", BlueTarget);
+      colorChooser.addOption("Green", GreenTarget);
+      colorChooser.addOption("Red", RedTarget);
+      colorChooser.addOption("Yellow", YellowTarget);
+      
+      dashboardTab.add(colorChooser)
+        .withWidget(BuiltInWidgets.kComboBoxChooser)
+        .withPosition(0, 0)
+        .withSize(4,2);
+  }
 
     public ColorSensor() {
         m_colorMatcher.addColorMatch(BlueTarget);
         m_colorMatcher.addColorMatch(GreenTarget);
         m_colorMatcher.addColorMatch(RedTarget);
         m_colorMatcher.addColorMatch(YellowTarget);
-
-        colorChooser.setDefaultOption("Blue", BlueTarget);
-        colorChooser.addOption("Green", GreenTarget);
-        colorChooser.addOption("Red", RedTarget);
-        colorChooser.addOption("Yellow", YellowTarget);
-        // etc.
-        SmartDashboard.putData("Given Color", colorChooser);
+        createColorChooser();
     }
 
     @Override
     public void periodic() {
         Color detectedColor = m_colorSensor.getColor();
-        Color givenColor = colorChooser.getSelected();
+        Color givenColor;
+        if(DriverStation.getInstance().isFMSAttached()){
+          givenColor = getColorMatch(); // this really shouldn't be called periodically 
+                                        // should be after stage 3 command but its just for testing
+                                        // bc if it is, it would just flood driverstation until we do stage 3
+        } else {
+          givenColor = colorChooser.getSelected();
+        }
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
         if (match.color == BlueTarget) {
@@ -64,6 +114,7 @@ public class ColorSensor extends SubsystemBase {
           colorString = "UNKOWN";
         }
 
+        // for testing chooser is working
         String givenString;
         if (givenColor == BlueTarget) {
           givenString = "BLUE";
@@ -79,7 +130,7 @@ public class ColorSensor extends SubsystemBase {
 
         SmartDashboard.putNumber("Confidence", match.confidence);
         SmartDashboard.putString("Detected Color", colorString);
-        SmartDashboard.putString("Given Color (test)", givenString);
+        SmartDashboard.putString("Given Color", givenString);
     }
 
     public String getColor() {
