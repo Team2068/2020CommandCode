103c56135556d8f5c956dfd89d3dda112d1d28b6
Andrew Schineller <schineaj23@pwcs-edu.org>
2020-02-10 16:40:22 -0500

    started working on gyro, no signal from port

--- src/main/java/frc/robot/subsystems/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/Gyroscope.java
@@ -4,3 +4,24 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Gyroscope extends SubsystemBase {
+    
+    private final Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+
+    public Gyroscope(){
+    }
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
+    }
+}
