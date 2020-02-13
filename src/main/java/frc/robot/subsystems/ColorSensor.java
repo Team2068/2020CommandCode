4d62dc6128333d6726d016e6bbe3d3f588587660
Andrew Schineller <schineaj23@pwcs-edu.org>
2020-02-13 16:03:04 -0500

    updated code and made passing

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -6,9 +6,15 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.util.Color;
@@ -23,24 +29,69 @@ public class ColorSensor extends SubsystemBase {
     private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
     private final ColorMatch m_colorMatcher = new ColorMatch();
 
+    private final Color NullTarget = ColorMatch.makeColor(0.0, 0.0, 0.0);
     private final Color BlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
     private final Color GreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
     private final Color RedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
     private final Color YellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-    String colorString;
-    
+    private String colorString;
+    private SendableChooser<Color> colorChooser = new SendableChooser<>();
+    private ShuffleboardTab dashboardTab = Shuffleboard.getTab("SmartDashboard");
+    private Color detectedColor = ColorMatch.makeColor(0.0, 0.0, 0.0);
+
+    private Color getColorMatch(){
+      String gameData = DriverStation.getInstance().getGameSpecificMessage();
+      if(gameData.length() > 0)
+      {
+        switch (gameData.charAt(0))
+        {
+          case 'B' : return BlueTarget;
+          case 'G' : return GreenTarget;
+          case 'R' : return RedTarget;
+          case 'Y' : return YellowTarget;
+          default :
+            DriverStation.reportWarning("Corrupt Color Data Recieved from FMS", false);
+            break;
+        }
+      } else {
+        DriverStation.reportWarning("No Color Data Receieved from FMS", false);
+      }
+      return NullTarget;
+    }
+
+    private void createColorChooser(){
+      SendableRegistry.add(colorChooser, "Color Chooser");
+      SendableRegistry.setName(colorChooser, "Color Chooser");
+
+      colorChooser.setDefaultOption("None", NullTarget);   
+      colorChooser.addOption("Blue", BlueTarget);
+      colorChooser.addOption("Green", GreenTarget);
+      colorChooser.addOption("Red", RedTarget);
+      colorChooser.addOption("Yellow", YellowTarget);
+      
+      dashboardTab.add(colorChooser)
+        .withWidget(BuiltInWidgets.kComboBoxChooser)
+        .withSize(1,1);
+    }
+
     public ColorSensor() {
         m_colorMatcher.addColorMatch(BlueTarget);
         m_colorMatcher.addColorMatch(GreenTarget);
         m_colorMatcher.addColorMatch(RedTarget);
         m_colorMatcher.addColorMatch(YellowTarget);
+        createColorChooser();
     }
 
     @Override
     public void periodic() {
-        Color detectedColor = m_colorSensor.getColor();
-
+        detectedColor = m_colorSensor.getColor();
+        Color givenColor;
+        if(DriverStation.getInstance().isFMSAttached()){
+          givenColor = getColorMatch();
+        } else {
+          givenColor = colorChooser.getSelected();
+        }
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
         if (match.color == BlueTarget) {
@@ -54,12 +105,17 @@ public class ColorSensor extends SubsystemBase {
         } else {
           colorString = "UNKOWN";
         }
-
+        
         SmartDashboard.putNumber("Confidence", match.confidence);
         SmartDashboard.putString("Detected Color", colorString);
     }
 
-    public String getColor() {
-        return colorString;
+    public Color getSensorColor() {
+      return detectedColor;
+    }
+
+    public boolean isSameColor(Color c1, Color c2) {
+      ColorMatchResult match = m_colorMatcher.matchClosestColor(c1);
+      if(match.color == c2) return true; else return false;
     }
 }
